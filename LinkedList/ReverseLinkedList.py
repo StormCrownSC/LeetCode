91d344ca7b59a3e12bae4ca66643138fb36cf464
Denis Sirotkin <sirotkin@denis-02.ru>
2023-01-14 14:01:59 +0300

    Create ReverseLinkedList.py

---
+++ LinkedList/ReverseLinkedList.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverseList(self, list):
+        return self.reverse(list)
+    
+    def longestPalindrome(self, s: str) -> str:
+        pass
+    
+    def reverse(self, head):
+        last = None
+        val = head
+        while val:
+            tmp = val.next
+            val.next = last
+            last = val
+            val = tmp
+        return last
+    
