50a53141be1edb91b5dfd13828bdf37217962161
Denis Sirotkin <sirotkin@denis-02.ru>
2023-01-14 13:43:41 +0300

    Create PlusOne.py

---
+++ Array/PlusOne.py
@@ -0,0 +1,16 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if len(digits) == 0:
+            return [1]
+        digits[-1] += 1
+        if digits[-1] > 9:
+            digits[-1] -= 10
+            digits = self.plusOne(digits[:-1]) + digits[-1:]
+
+        return digits
+
+"""
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+Increment the large integer by one and return the resulting array of digits
+"""
